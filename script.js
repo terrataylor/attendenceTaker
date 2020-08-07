41cf4076abe84a0958aad5e67ca89e7aedbb5fd5
Terra <terra.taylor@gmail.com>
2020-08-07 10:13:40 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$("form").submit(function (evt) {
+    evt.preventDefault();
+    let names = $("#names").val().trim().split("\n");
+    let chat = $("#chat").val();
+    chat = chat.toLowerCase();
+    let students = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i].toLowerCase().trim();
+        if (name != "") {
+            if (chat.includes(name)) {
+                students.push({ name: name, present: true });
+            } else {
+                students.push({ name: name, present: false });
+            }
+        }
+    }
+    let html = [];
+    for (x in students) {
+        html.push(`<tr>
+        <th scope="row">${x}</th>
+        <td>${students[x].name}</td>
+        <td></td>
+        <td>${students[x].present}</td>
+      </tr>`);
+    }
+    $("tbody").html(html.toString());
+})
+
